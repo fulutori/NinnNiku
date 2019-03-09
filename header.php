4b9764173a7a1af0eb29d5163dbd3e19b8a34436
ikezaki_M\ikezaki <BS116001@sun.ac.jp>
2019-03-10 06:05:52 +0900

    Update header.php

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 	<div class="container">
 	<a class="navbar-brand text-white" href="index.php">ブランド</a>
 	<div class="navbar-nav ml-auto mt-2 ">
-		<a class="nav-item nav-link text-light " href="mypage.php"><?php if ($user_id!="") echo $user_id; ?></a>
+		<a class="nav-item nav-link text-light " href="mypage.php"><b><?php if ($user_id!="") echo $user_id; ?></b></a>
     </div>
 	</div>
 </nav>
