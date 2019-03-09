f4d7a0f932f6b5d17c92ef0ee97a3adbf638fc34
fulutori <miyatori25@gmail.com>
2019-03-10 02:49:23 +0900

    Update get_ramen.php

--- get_ramen.php
+++ get_ramen.php
@@ -10,15 +10,29 @@ $pdo = new PDO($dsn, $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMO
 $ido = $_POST['ido'];
 $keido = $_POST['keido'];
 $user_id = $_POST['user_id'];
-$sql = 'SELECT * FROM shop WHERE abs(latitude - ?) < 0.0003 AND abs(longitude - ?) < 0.0005 ORDER BY abs(latitude - ?) + abs(longitude - ?) limit 1';
+# WHERE abs(latitude - ?) < 0.0003 AND abs(longitude - ?) < 0.0005
+$sql = 'SELECT * FROM shop ORDER BY abs(latitude - ?) + abs(longitude - ?) limit 1';
 $stmt = $pdo->prepare($sql);
-if ($stmt->execute([$ido, $keido, $ido, $keido])) {
+if ($stmt->execute([$ido, $keido])) {
 	$result = $stmt->fetch(PDO::FETCH_ASSOC);
 	$shop_name = $result['shop_name'];
+	$ramen_num = $result['ramen_num'];
 	if ($shop_name == "") {
 		echo "ここはラーメン屋ではありません。";
 	} else {
-		echo $shop_name;	
+		$sql = "INSERT INTO log(id,ramen_num) VALUES(?, ?)";
+		$stmt2 = $pdo->prepare($sql);
+		if ($stmt2->execute([$user_id, $ramen_num])) {
+			$sql = "SELECT COUNT(*) FROM log WHERE id=? AND ramen_num=?";
+			$stmt3 = $pdo->prepare($sql);
+			$stmt3->execute([$user_id, $ramen_num]);
+			$result3 = $stmt3->fetchColumn();
+			if ($result3 == 1) {
+				echo $shop_name."(初)";
+			} else {
+				echo $shop_name;	
+			}
+		}
 	}
 }
 ?>
