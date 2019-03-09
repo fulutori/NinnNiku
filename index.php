d4b958081d8e5b41bd7820b2bc4ccb2e2b2a62e3
ikezaki_M\ikezaki <BS116001@sun.ac.jp>
2019-03-10 08:27:33 +0900

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $user_id = $id;
 <html>
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>マイページ</title>
+<title>トップページ</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
