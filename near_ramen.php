65196c3e9e8e9ff66f8f50e3d9017a3907ed5e0c
fulutori <miyatori25@gmail.com>
2019-03-10 01:41:38 +0900

    Update near_ramen.php

--- near_ramen.php
+++ near_ramen.php
@@ -13,22 +13,19 @@ $user_id = $_POST['user_id'];
 
 #latitude(緯度)は0.00027778で31メートルぐらい
 #longitude(経度)は計算が難しいので緯度よりも多めに指定する
-$sql = "SELECT * FROM shop WHERE abs(latitude - ".$ido.") < 0.00015 AND abs(longitude - ".$keido.") < 0.0003 ORDER BY abs(latitude - ".$ido.") + abs(longitude - ".$keido.") limit 3";
+$sql = "SELECT * FROM shop WHERE abs(latitude - ".$ido.") < 0.015 AND abs(longitude - ".$keido.") < 0.03 ORDER BY abs(latitude - ".$ido.") + abs(longitude - ".$keido.") limit 3";
 $stmt = $pdo->query($sql);
 if ($stmt) {
 	$rank=1;
 	foreach ($stmt as $row) {
 		$shop_name = $row['shop_name'];
-		if ($shop_name == "") {
-			echo "近くにラーメン屋はありません。";
-			break;
-		}
 		echo "<div class=\"card container p-3\"><class class=\"row\">
 		<div class=\"col-4\">".$shop_name."</div>
 		</div>";
 		$rank++;
 	}
-} else {
-	echo "hoge";
+	if ($rank == 1) {
+		echo "近くにラーメン店はありません。";
+	}
 }
 ?>
