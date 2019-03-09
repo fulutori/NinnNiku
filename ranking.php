2883471d67aeb71a3b3665e9cbfd76ad6feb0ec3
ikezaki_M\ikezaki <BS116001@sun.ac.jp>
2019-03-10 01:07:17 +0900

    Update ranking.php

--- ranking.php
+++ ranking.php
@@ -17,21 +17,7 @@ $user_id = $_SESSION['user'];
 <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-maincolor ">
-	<div class="container">
-	<a class="navbar-brand text-white" href="index.php">ブランド</a>
-	<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="ナビゲーションの切替">
-		<span class="navbar-toggler-icon"></span>
-  	</button>
-	
-	<div class="collapse navbar-collapse " id="Navber">
-		<div class="my-2 my-lg-0 ml-auto">
-			<a class="btn btn-outline-light " href="mypage.php"><?php if ($user_id!="") echo $user_id; ?></a>
-		</div>
-	</div>
-	</div>
-</nav>
-
+<?php include("header.php"); ?>
 
 <div class="container">
 <h2 class="m-1">ランキング</h2>
