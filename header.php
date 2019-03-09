17cf737315739414f6d742d5b8950351f4c428cb
ikezaki_M\ikezaki <BS116001@sun.ac.jp>
2019-03-10 02:18:35 +0900

    Update header.php

--- header.php
+++ header.php
@@ -1,14 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-maincolor shadow navbar-light fixed-top">
 	<div class="container">
 	<a class="navbar-brand text-white" href="index.php">ブランド</a>
-	<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="ナビゲーションの切替">
-		<span class="navbar-toggler-icon"></span>
-  	</button>
-	
-	<div class="collapse navbar-collapse " id="Navber">
-		<div class="my-2 my-lg-0 ml-auto">
-			<a class="btn btn-outline-light " href="mypage.php"><?php if ($user_id!="") echo $user_id; ?></a>
-		</div>
-	</div>
+	<div class="navbar-nav ml-auto mt-2 ">
+		<a class="nav-item nav-link text-light " href="mypage.php"><?php if ($user_id!="") echo $user_id; ?></a>
+    </div>
 	</div>
 </nav>
