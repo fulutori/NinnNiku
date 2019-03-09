8b6d7bd63f39586b1ae0c2d02b6822f7ad43dcb3
ikezaki_M\ikezaki <BS116001@sun.ac.jp>
2019-03-10 01:07:12 +0900

    Update index.php

--- index.php
+++ index.php
@@ -39,20 +39,7 @@ $user_id = $id;
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-maincolor ">
-	<div class="container">
-	<a class="navbar-brand text-white" href="index.php">ブランド</a>
-	<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="ナビゲーションの切替">
-		<span class="navbar-toggler-icon"></span>
-  	</button>
-	
-	<div class="collapse navbar-collapse " id="Navber">
-		<div class="my-2 my-lg-0 ml-auto">
-			<a class="btn btn-outline-light " href="mypage.php"><?php if ($user_id!="") echo $user_id; ?></a>
-		</div>
-	</div>
-	</div>
-</nav>
+<?php include("header.php"); ?>
 
 <div class="container">
 <h2 class="m-1">トップページ</h2>
