e8bd15a394a3435bb599fbab51b26276c98d4563
ikezaki_M\ikezaki <BS116001@sun.ac.jp>
2019-03-10 05:53:33 +0900

    Update ninnniku.php

--- ninnniku.php
+++ ninnniku.php
@@ -18,6 +18,8 @@ if(!isset($_SESSION['user'])) {
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+<!--fontawesome-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
 <body style="padding-top:4.5rem;">
 <?php include("header.php"); ?>
