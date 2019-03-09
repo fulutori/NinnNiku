abf0d1dca9c1c766e462f8d246f17eb38ca48d27
ikezaki_M\ikezaki <BS116001@sun.ac.jp>
2019-03-10 00:50:49 +0900

    メニューバー編集

--- ninnniku.php
+++ ninnniku.php
@@ -20,16 +20,16 @@ if(!isset($_SESSION['user'])) {
 <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-maincolor">
+<nav class="navbar navbar-expand-lg navbar-light bg-maincolor ">
 	<div class="container">
-	<a class="navbar-brand" href="index.php">ブランド</a>
+	<a class="navbar-brand text-white" href="index.php">ブランド</a>
 	<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="ナビゲーションの切替">
 		<span class="navbar-toggler-icon"></span>
   	</button>
 	
 	<div class="collapse navbar-collapse " id="Navber">
 		<div class="my-2 my-lg-0 ml-auto">
-		<?php if ($user_id!="") echo $user_id; ?>
+			<a class="btn btn-outline-light " href="mypage.php"><?php if ($user_id!="") echo $user_id; ?></a>
 		</div>
 	</div>
 	</div>
