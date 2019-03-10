aeadc15a4c6302fd916a39b6dc79cc8fb560af1f
ikezaki_M\ikezaki <BS116001@sun.ac.jp>
2019-03-10 15:43:55 +0900

    Update ranking.php

--- ranking.php
+++ ranking.php
@@ -15,6 +15,8 @@ $user_id = $_SESSION['user'];
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+<link rel="shortcut icon" href="assets/favicon.ico" />
+<link rel="icon" type="image/vnd.microsoft.icon" href="assets/favicon.ico">
 </head>
 <body style="padding-top:4.5rem;">
 <?php include("header.php"); ?>
